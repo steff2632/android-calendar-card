45b3f5cd02e567253c89826725344f0f13caeac4
steff2632 <stefn.may@bizarreware.co.uk>
2014-01-29 18:12:15 +0000

    implemented button clicks

--- calendar-card-sample/src/com/wt/calendarcardsample/Sample1.java
+++ calendar-card-sample/src/com/wt/calendarcardsample/Sample1.java
@@ -1,11 +1,14 @@
 package com.wt.calendarcardsample;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.wt.calendarcard.CalendarCard;
@@ -22,6 +25,33 @@ public class Sample1 extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sample1);
 		mCalendarCard = (CalendarCard)findViewById(R.id.calendarCard1);
+		
+		final Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.MONTH, 1);
+		mCalendarCard.setDateDisplay(calendar);
+		mCalendarCard.notifyChanges();
+		
+		Button btn = (Button)findViewById(R.id.button1);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				calendar.add(Calendar.MONTH, 1);
+				mCalendarCard.setDateDisplay(calendar);
+			}
+		});
+		
+		Button button = (Button) findViewById(R.id.button2);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				calendar.add(Calendar.MONTH, -1);
+				mCalendarCard.setDateDisplay(calendar);
+				
+			}
+		});
+		
 		mCalendarCard.setOnCellItemClick(new OnCellItemClick() {
 			@Override
 			public void onCellClick(View v, CardGridItem item) {
